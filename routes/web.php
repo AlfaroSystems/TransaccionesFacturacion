0b909a77b597c40fbb591956aaa0bf309b375cc0
Alfaro Systems <131634183+AlfaroSystems@users.noreply.github.com>
2026-07-26 11:31:18 -0600

    Redirigir la ruta raiz (/) a la pagina de login

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\RoleController;
 use App\Http\Controllers\EmpleadoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
